ca13e8df5be1dbbc01f22c7548164eee0a4d81be
Andres-jr69 <98850844+Andres-jr69@users.noreply.github.com>
2023-07-08 13:51:58 -0500

    Fin seccion 4 - Router

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+
+
+
+@NgModule({
+  declarations: [
+    BreadcrumbsComponent,
+    HeaderComponent,
+    SidebarComponent,
+  ],
+  imports: [
+    CommonModule
+  ], exports: [
+    BreadcrumbsComponent,
+    HeaderComponent,
+    SidebarComponent,
+  ]
+})
+export class SharedModule { }
